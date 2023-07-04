a7fdc00c6bf7d091e91d12cf4ab9c079745c7e90
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2023-07-04 21:37:40 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-    return <h1>Test - My first webpage. We'll get there</h1>
+  return <h1>Test - My first webpage. We'll get there</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
+}
 
 export default Home;

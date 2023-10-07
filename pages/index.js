d326cba203009398070b8ab45ea99e87216b539f
YuriCatramby <yuri.catramby@hotmail.com>
2023-10-07 10:58:04 -0300

    redo

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return <h1>WIP - My first webpage. We'll get there</h1>;
+}
+
+// function test() {
+//   console.log("test");
+// }
+
+// function test2() {
+//   console.log("wrong identation");
+// }
+
+export default Home;

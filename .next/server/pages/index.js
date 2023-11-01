733d8db76e28c3eee996d4da2b2e72f6aa5e79c5
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2023-11-01 12:37:05 +0000

    add database.js and .env

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"WIP - My first webpage. We'll get there\"\n    }, void 0, false, {\n        fileName: \"/workspaces/clone-tabnews/pages/index.js\",\n        lineNumber: 2,\n        columnNumber: 10\n    }, this);\n}\n// function test() {\n//   console.log(\"test\");\n// }\n// function test2() {\n//   console.log(\"wrong identation\");\n// }\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTztJQUNkLHFCQUFPLDhEQUFDQztrQkFBRzs7Ozs7O0FBQ2I7QUFFQSxvQkFBb0I7QUFDcEIseUJBQXlCO0FBQ3pCLElBQUk7QUFFSixxQkFBcUI7QUFDckIscUNBQXFDO0FBQ3JDLElBQUk7QUFFSixpRUFBZUQsSUFBSUEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2Nsb25lLXRhYm5ld3MvLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIEhvbWUoKSB7XG4gIHJldHVybiA8aDE+V0lQIC0gTXkgZmlyc3Qgd2VicGFnZS4gV2UnbGwgZ2V0IHRoZXJlPC9oMT47XG59XG5cbi8vIGZ1bmN0aW9uIHRlc3QoKSB7XG4vLyAgIGNvbnNvbGUubG9nKFwidGVzdFwiKTtcbi8vIH1cblxuLy8gZnVuY3Rpb24gdGVzdDIoKSB7XG4vLyAgIGNvbnNvbGUubG9nKFwid3JvbmcgaWRlbnRhdGlvblwiKTtcbi8vIH1cblxuZXhwb3J0IGRlZmF1bHQgSG9tZTtcbiJdLCJuYW1lcyI6WyJIb21lIiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();

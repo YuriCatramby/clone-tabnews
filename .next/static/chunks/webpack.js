73dd1c5f86c68f5b3a916bbd778327e6f396aa73
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2023-12-04 11:13:16 +0000

    jsconfig.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac2ee9453334dfaf"; }
+/******/ 		__webpack_require__.h = function() { return "6dd7f9dd32a23930"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

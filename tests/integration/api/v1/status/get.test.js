12503d849a25c0af7492b9a3b66ddbf41629d107
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2023-12-12 22:08:12 +0000

    refac

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,8 +3,16 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parserUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parserUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(
+    parseInt(responseBody.dependencies.database.max_connections, 10)
+  ).toBeGreaterThanOrEqual(
+    // parseInt(responseBody.dependencies.database.opened_connections, 10)
+    responseBody.dependencies.database.opened_connections
+  );
 });

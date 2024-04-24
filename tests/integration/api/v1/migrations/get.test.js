131284f29fff4a228e90c4d204506e854d9a30fd
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2024-04-24 20:30:29 +0000

    test staging

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -11,8 +11,6 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  ç;
-
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 });

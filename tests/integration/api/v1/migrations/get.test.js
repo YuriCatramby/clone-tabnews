0c3e4ec2d707889f1a033adf8b8199e500408d4a
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2024-09-13 11:25:55 +0000

    test: refactor text

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,7 +8,7 @@ beforeAll(async () => {
 // await database.query("drop schema public cascade; create schema public;");
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    test("Running pending migrations", async () => {
+    test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
 

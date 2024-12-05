dc0bdb8ba928683857b7a0590f64a2f10bc27b16
Yuri Catramby <69281640+YuriCatramby@users.noreply.github.com>
2024-12-05 11:54:04 +0000

    fix: fix database status

--- .next/static/webpack/webpack.d1d2629a2d91f28d.hot-update.js
+++ .next/static/webpack/webpack.d1d2629a2d91f28d.hot-update.js
@@ -7,19 +7,12 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"](
-  "webpack",
-  {},
-  /******/ function (__webpack_require__) {
-    // webpackRuntimeModules
-    /******/ /* webpack/runtime/getFullHash */
-    /******/ !(function () {
-      /******/ __webpack_require__.h = function () {
-        return "818951909956ef62";
-      };
-      /******/
-    })();
-    /******/
-    /******/
-  },
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "d5a252d8941edc45"; }
+/******/ }();
+/******/ 
+/******/ }
 );
